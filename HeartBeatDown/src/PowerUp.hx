62720c585f09416916eab99c05dae111fcfb3570
David Chin <chin@hawaii.edu>
2013-01-26 11:51:26 -1000

    added class files for baddies and power ups

---
+++ HeartBeatDown/src/PowerUp.hx
@@ -0,0 +1,25 @@
+import flambe.Component;
+
+class PowerUp extends Component
+{
+	public function new()
+	{
+		
+	}
+}
+
+class Adrenaline extends PowerUp
+{
+		public function new()
+	{
+		
+	}
+}
+
+class Vagusstoff extends PowerUp
+{
+		public function new()
+	{
+		
+	}
+}
