8cb1e2fb67fd62308d5bcf17f39a213ff6d4dd29
Jon Borgonia <remix@dhcp-168-105-86-87.wireless.manoa.hawaii.edu>
2013-01-25 19:47:28 -1000

    initial flambe project

---
+++ HeartBeatDown/src/HeartBeatDownMain.hx
@@ -0,0 +1,21 @@
+import flambe.asset.AssetPack;
+import flambe.asset.Manifest;
+import flambe.System;
+
+class HeartBeatDownMain
+{
+    private static function onSuccess (pack :AssetPack)
+    {
+    }
+
+    private static function main ()
+    {
+        // Wind up all platform-specific stuff
+        System.init();
+
+        // Load up the compiled pack in the assets directory named "bootstrap"
+        var manifest = Manifest.build("bootstrap");
+        var loader = System.loadAssetPack(manifest);
+        loader.get(onSuccess);
+    }
+}
