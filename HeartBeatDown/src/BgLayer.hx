b429bf89a0931e2ab85ad497f2720cead875ee2b
Russell <veas@dhcp-168-105-85-223.wireless.manoa.hawaii.edu>
2013-01-26 15:14:05 -1000

    Added zooming animations and Rob's artery texture.

---
+++ HeartBeatDown/src/BgLayer.hx
@@ -0,0 +1,19 @@
+import flambe.display.FillSprite;
+import flambe.Component;
+import flambe.System;
+import flambe.Entity;
+import flambe.display.Sprite;
+import flambe.script.Script;
+import flambe.script.Repeat;
+import flambe.script.CallFunction;
+
+class BgLayer extends Component {
+
+	private var image:FillSprite;
+	public var entity:Entity;
+	
+	public function new () 
+	{
+		entity = new Entity().add(new FillSprite(0x000000, System.stage.width, System.stage.height));
+	}
+}
