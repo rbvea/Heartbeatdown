40d07a736e892ca11b0c4375a58a1a7de0879ebf
Jon Borgonia <jon@gomagames.com>
2013-01-26 15:36:52 -1000

    TouchController uses unified System.pointer
    so we can debug in desktop browser

--- HeartBeatDown/src/TouchController.hx
+++ HeartBeatDown/src/TouchController.hx
@@ -1,6 +1,6 @@
 import flambe.System;
-import flambe.input.Touch;
-import flambe.input.TouchPoint;
+import flambe.input.Pointer;
+import flambe.input.PointerEvent;
 import flambe.input.Key;
 
 
@@ -8,26 +8,37 @@ class TouchController extends AbstractController
 {
   private var tempTrace:flambe.display.TextSprite;
 
+  private var touching:Bool;
+
   public function new(game)
   {
     super(game);
-    System.touch.down.connect(touchDown);
-    System.touch.up.connect(touchUp);
-    System.touch.move.connect(touchMove);
+    
+    touching = false;
 
+    System.pointer.down.connect(touchDown);
+    System.pointer.up.connect(touchUp);
+    System.pointer.move.connect(touchMove);
+    
     tempTrace = new flambe.display.TextSprite(new flambe.display.Font(HeartBeatDownMain.pack, "redrum72"), "DEBUG");
     game.layer_ui.add(tempTrace);
   }
-  private inline function touchDown(point:TouchPoint):Void
+
+  private inline function touchDown(point:PointerEvent):Void
   {
     tempTrace.text = Std.string(point.viewX) + " " + Std.string(point.viewY);
+    touching = true;
   }
-  private inline function touchUp(point:TouchPoint):Void
+  private inline function touchUp(point:PointerEvent):Void
   {
     tempTrace.text = Std.string(point.viewX) + " " + Std.string(point.viewY);
+    touching = false;
   }
-  private inline function touchMove(point:TouchPoint):Void
+  private inline function touchMove(point:PointerEvent):Void
   {
-    tempTrace.text = Std.string(point.viewX) + " " + Std.string(point.viewY);
-  } 
+    if(touching){
+      tempTrace.text = Std.string(point.viewX) + " " + Std.string(point.viewY);
+    } 
+  }
+
 }
