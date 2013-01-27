cf42de3afa926e38df0dcac68a7425f8145fe5b6
David Chin <chin@hawaii.edu>
2013-01-26 15:44:20 -1000

    added random placement of baddie within round artery.

--- HeartBeatDown/src/Bacteria.hx
+++ HeartBeatDown/src/Bacteria.hx
@@ -1,9 +1,17 @@
+import flambe.Entity;
+import flambe.display.Sprite;
+import flambe.display.ImageSprite;
+import flambe.System;
+
 class Bacteria extends Baddie
 {
+
 		public function new(arteryRadius :Float)
 	{
+		height = 200;
+		width = 200;
+		entity = new Entity()
+			.add(new ImageSprite(HeartBeatDownMain.pack.getTexture("Bacteria")));
 		super(arteryRadius);
-		height = 100;
-		width = 100;
 	}
 }
