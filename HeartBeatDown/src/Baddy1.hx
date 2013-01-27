9cb6ce3797c4a10ee13de8c7997142e32821adca
Russell <veas@dhcp-168-105-85-223.wireless.manoa.hawaii.edu>
2013-01-27 09:50:31 -1000

    Merging Dr. Chin's changes

--- HeartBeatDown/src/Baddy1.hx
+++ HeartBeatDown/src/Baddy1.hx
@@ -18,7 +18,7 @@ class Baddy1 extends Baddy
     entity = new Entity()
 		.add(new MoviePlayer(new Library(HeartBeatDownMain.pack, "Bacteria-Simple")).loop("Bacteria"));
 		
-	var randX = Std.random(204) + 400;
+	var randX = Std.random(408) + 308;
 	var randY = Std.random(154) + 307;
 	var stage_width = System.stage.width;
 	var stage_height  = System.stage.height;
@@ -28,22 +28,18 @@ class Baddy1 extends Baddy
 	var body = entity.get(Sprite);
 
 	if(randX <  stage_width / 2) {
-		if(randY < stage_width / 2) { // top left
-			trace("top left");
+		if(randY < stage_height / 2) { // top left
 			body.anchorX._ = 3 * (System.stage.width / 4);
 			body.anchorY._ = 3 * (System.stage.height / 4);
 		} else {  // bottom left
-			trace("bottom left");
 			body.anchorX._ = System.stage.width / 4;
 			body.anchorY._ = 3 * (System.stage.height / 4);
 		}
 	} else {
-		if(randY < stage_width / 2) { // top right 
-			trace("top right");
+		if(randY < stage_height / 2) { // top right 
 			body.anchorX._ = System.stage.width / 4;
 			body.anchorY._ = 3 * (System.stage.height / 4);
 		} else {  // bottom right
-			trace("bottom right");
 			body.anchorX._ = System.stage.width / 4;
 			body.anchorY._ = System.stage.width / 4;
 		}
