567ecae357274ee9f8dc2bece38b02c6e330fa73
Jon Borgonia <jon@gomagames.com>
2013-01-27 13:55:02 -1000

    added hazard back in

--- HeartBeatDown/src/Game.hx
+++ HeartBeatDown/src/Game.hx
@@ -143,62 +143,64 @@ class Game extends Component
     var forkTicks = TICKS_PER_MAP_SEGMENT * (currentNode.pointArray.length - 1);
     tick++;
 
-	if(!reachedEnd) {
-    if(!forking_action && (tick * moveSpeed) % 90 == 0){
-      var lwall = new LayerWall(this);
-      layer_walls_list.add(lwall);
-      layer_walls.addChild(lwall.entity);
-    }
-
-    if (Std.random(75-(heart_rate*5)) == 0) {
-      // pick one of the 6 lanes
-      // add a hazard to it
-      var hazard = new Hazard(this);
-      hazards.add(hazard);
-      layer_game.addChild(hazard.entity);
-    if(tick == forkTicks-40){
-      var layer_fork = new LayerFork(this);
-      layer_walls.add(layer_fork);
-      layer_walls.addChild(layer_fork.entity);
-      forking_action = true;
-    }
-    if(forking_action && tick==forkTicks){
-      forking_action = false;
-      tick = 0;
-    }
-
-		if(tick == baddy_random && !forking_action) {
-			layer_game.addChild(new Baddy1(this).entity);
-			baddy_random = Std.random(480);
-		}
-
-    // increase all layer wall scales
-    for(lw in layer_walls_list){
-      if(lw.image.scaleX._<5){
-        lw.acceleration += (moveSpeed*dt/100);
-        lw.image.setScale(lw.image.scaleX._+lw.acceleration);
-      }else{
-        lw.entity.dispose();
-        //image.dispose();
-        layer_walls_list.remove(lw);
+  	if(!reachedEnd) {
+      if(!forking_action && (tick * moveSpeed) % 90 == 0){
+        var lwall = new LayerWall(this);
+        layer_walls_list.add(lwall);
+        layer_walls.addChild(lwall.entity);
       }
-    }		
 
-	} else if(tick < 500) { //rapid spawning baddies
-		if(Std.random(5) == 0) {
-			layer_game.addChild(new FinalBaddy(this).entity);
-		}
-	} else { //fadeOut
-			var final = new Entity().add(
-				new FillSprite(0xFFFFFF, System.stage.width, System.stage.height)
-			);
-			var whiteout = final.get(FillSprite);
-			whiteout.alpha._ = 0;
-			System.root.addChild(final);
-			whiteout.alpha.animateTo(1, 7);
-			haxe.Timer.delay(HeartBeatDownMain.cleanup, 7000);
-	}
-  updateMiniMap();
+      if (Std.random(75-(heart_rate*5)) == 0) {
+        // pick one of the 6 lanes
+        // add a hazard to it
+        var hazard = new Hazard(this);
+        hazards.add(hazard);
+        layer_game.addChild(hazard.entity);
+      }
+      
+      if(tick == forkTicks-40){
+        var layer_fork = new LayerFork(this);
+        layer_walls.add(layer_fork);
+        layer_walls.addChild(layer_fork.entity);
+        forking_action = true;
+      }
+      if(forking_action && tick==forkTicks){
+        forking_action = false;
+        tick = 0;
+      }
+
+  		if(tick == baddy_random && !forking_action) {
+  			layer_game.addChild(new Baddy1(this).entity);
+  			baddy_random = Std.random(480);
+  		}
+
+      // increase all layer wall scales
+      for(lw in layer_walls_list){
+        if(lw.image.scaleX._<5){
+          lw.acceleration += (moveSpeed*dt/100);
+          lw.image.setScale(lw.image.scaleX._+lw.acceleration);
+        }else{
+          lw.entity.dispose();
+          //image.dispose();
+          layer_walls_list.remove(lw);
+        }
+      }		
+
+  	} else if(tick < 500) { //rapid spawning baddies
+  		if(Std.random(5) == 0) {
+  			layer_game.addChild(new FinalBaddy(this).entity);
+  		}
+  	} else { //fadeOut
+  			var final = new Entity().add(
+  				new FillSprite(0xFFFFFF, System.stage.width, System.stage.height)
+  			);
+  			var whiteout = final.get(FillSprite);
+  			whiteout.alpha._ = 0;
+  			System.root.addChild(final);
+  			whiteout.alpha.animateTo(1, 7);
+  			haxe.Timer.delay(HeartBeatDownMain.cleanup, 7000);
+  	}
+    updateMiniMap();
   }
 
   private function makeMiniMap(): Void
