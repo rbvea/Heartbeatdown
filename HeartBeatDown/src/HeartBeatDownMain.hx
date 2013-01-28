c922fa8956afd4cbe66028fa19b5cd7ae45814db
Jon Borgonia <jon@gomagames.com>
2013-01-27 14:11:10 -1000

    adding UI assets added start title screen

--- HeartBeatDown/src/HeartBeatDownMain.hx
+++ HeartBeatDown/src/HeartBeatDownMain.hx
@@ -1,14 +1,24 @@
 import flambe.asset.AssetPack;
 import flambe.asset.Manifest;
 import flambe.System;
+import flambe.display.ImageSprite;
+import flambe.input.PointerEvent;
 
 class HeartBeatDownMain
 {
   public static var pack :AssetPack;
 
+  private static var title_screen:ImageSprite;
     private static function onSuccess (pack :AssetPack)
     {
         HeartBeatDownMain.pack = pack;
+        // load title screen
+        title_screen = new ImageSprite(pack.getTexture("ui/ui_title_screen"));
+        title_screen.pointerUp.connect(start_game);
+        System.root.add(title_screen);
+    }
+    private static inline function start_game(event:PointerEvent):Void{
+        System.root.remove(title_screen);
         System.root.add(new Game());
     }
 
